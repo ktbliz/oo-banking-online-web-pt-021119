254ecb0e2b6ad46308da4e71d67a7f78949ad6e9
Katie Blizzard <kblizzard11@gmail.com>
2019-03-23 21:16:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount 
+    @status = "pending"
+  end 
+  
+  def valid?
+    
+  end 
+  
+  def execute_transaction 
+    
+  end 
+  
+  def reverse_transfer
+    
+  end 
+  
+  
 end
